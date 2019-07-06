8785b96e0559900ff98df228c9161f0d0af5df27
musicq <liang.kui@hotmail.com>
2019-07-07 00:48:58 +0800

    feat: 给卡片增加 ink 效果

--- lib/src/widgets/CardPiece/CardPiece.dart
+++ lib/src/widgets/CardPiece/CardPiece.dart
@@ -5,43 +5,52 @@ class CardPiece extends StatelessWidget {
   final String img;
   final String title;
   final String subTitle;
+  final Function onTap;
 
   CardPiece({
     this.img,
     this.title,
     this.subTitle,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        padding: EdgeInsets.all(26),
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(12),
+      child: Ink(
         decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(12)),
-          boxShadow: [BoxShadow(blurRadius: 4, color: Colors.black12)],
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 4)],
         ),
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset(
-                img,
-                height: 45,
-                width: 45,
-                fit: BoxFit.fill,
-              ),
+        child: Container(
+          constraints: BoxConstraints.expand(width: 150, height: 180),
+          padding: const EdgeInsets.all(26),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    img,
+                    height: 45,
+                    width: 45,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Text(
+                  title,
+                  style: TextStyle(fontSize: 28, color: Colors.black),
+                ),
+                Text(
+                  subTitle,
+                  style: Style.tipText,
+                ),
+              ],
             ),
-            Text(
-              title,
-              style: TextStyle(fontSize: 28, color: Colors.black),
-            ),
-            Text(
-              subTitle,
-              style: Style.tipText,
-            ),
-          ],
+          ),
         ),
       ),
     );
