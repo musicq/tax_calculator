8db26fb04ddf3a26215b1866e257fb38bfbaf68d
musicq <liang.kui@hotmail.com>
2019-07-08 17:41:29 +0800

    feat: 调整月度计算界面样式

--- lib/src/views/MonthlyCalculation/MonthlyCalculation.dart
+++ lib/src/views/MonthlyCalculation/MonthlyCalculation.dart
@@ -4,6 +4,8 @@ import 'package:tax_calculator/src/widgets/ChipButton/ChipButton.dart';
 import 'package:tax_calculator/src/widgets/InputField/InputField.dart';
 import 'package:tax_calculator/src/widgets/MainContainer/MainContainer.dart';
 
+import 'MonthlyCalculation.style.dart' as MonthlyCalculationStyle;
+
 class MonthlyCalculation extends StatefulWidget {
   @override
   _MonthlyCalculationState createState() => _MonthlyCalculationState();
@@ -12,29 +14,49 @@ class MonthlyCalculation extends StatefulWidget {
 class _MonthlyCalculationState extends State<MonthlyCalculation> {
   PageController controller = PageController(initialPage: 0);
 
+  Widget itemWrap({Widget child}) {
+    return Container(
+      padding: MonthlyCalculationStyle.itemPadding,
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MainContainer(
       body: PageView(
         controller: controller,
         children: <Widget>[
-          Container(
-            child: Column(
-              children: <Widget>[
-                ChipButton(
-                  text: '更多',
-                  onTap: () {
-                    controller.nextPage(
-                      duration: Duration(milliseconds: 300),
-                      curve: Curves.linear,
-                    );
-                  },
-                ),
-                InputField(
-                  label: '输入税前薪资收入',
-                ),
-                IncomeOverview(income: 1000),
-              ],
+          SingleChildScrollView(
+            child: Container(
+              padding: MonthlyCalculationStyle.container,
+              child: Column(
+                children: <Widget>[
+                  Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: ChipButton(
+                          text: '更多',
+                          onTap: () {
+                            controller.nextPage(
+                              duration: Duration(milliseconds: 300),
+                              curve: Curves.easeInOut,
+                            );
+                          },
+                        ),
+                      ),
+                      itemWrap(
+                        child: InputField(
+                          label: '输入税前薪资收入',
+                        ),
+                      ),
+                    ],
+                  ),
+                  // 收入概览
+                  IncomeOverview(income: 1000),
+                ],
+              ),
             ),
           ),
           Text('Placeholder'),
---
+++ lib/src/views/MonthlyCalculation/MonthlyCalculation.style.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const container = EdgeInsets.all(20);
+
+const itemPadding = EdgeInsets.symmetric(vertical: 20);
