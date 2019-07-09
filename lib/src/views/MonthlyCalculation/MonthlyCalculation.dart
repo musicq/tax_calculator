92b0d568088d1df7cca534365e6ae3b295701878
musicq <liang.kui@hotmail.com>
2019-07-09 09:58:49 +0800

    refactor: 将月度计算 layout Stack 提出来

--- lib/src/views/MonthlyCalculation/MonthlyCalculation.dart
+++ lib/src/views/MonthlyCalculation/MonthlyCalculation.dart
@@ -6,6 +6,7 @@ import 'package:tax_calculator/src/widgets/InputField/InputField.dart';
 import 'package:tax_calculator/src/widgets/MainContainer/MainContainer.dart';
 
 import 'MonthlyCalculation.style.dart' as MonthlyCalculationStyle;
+import 'SpecialAttachment.dart';
 
 class MonthlyCalculation extends StatefulWidget {
   @override
@@ -17,13 +18,6 @@ class _MonthlyCalculationState extends State<MonthlyCalculation> {
   TextEditingController inputCtrl = TextEditingController();
   double excludeMoney = 4500;
 
-  Widget itemWrap({Widget child}) {
-    return Container(
-      padding: MonthlyCalculationStyle.itemPadding,
-      child: child,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return MainContainer(
@@ -33,23 +27,24 @@ class _MonthlyCalculationState extends State<MonthlyCalculation> {
           SingleChildScrollView(
             child: Container(
               padding: MonthlyCalculationStyle.container,
-              child: Column(
-                children: <Widget>[
-                  Stack(
-                    children: [
-                      Align(
-                        alignment: Alignment.topRight,
-                        child: ChipButton(
-                          text: '更多',
-                          onTap: () {
-                            controller.nextPage(
-                              duration: Duration(milliseconds: 300),
-                              curve: Curves.easeInOut,
-                            );
-                          },
-                        ),
-                      ),
-                      itemWrap(
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: ChipButton(
+                      text: '更多',
+                      onTap: () {
+                        controller.nextPage(
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.easeInOut,
+                        );
+                      },
+                    ),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        padding: MonthlyCalculationStyle.itemPadding,
                         child: InputField(
                           label: '输入税前薪资收入',
                           controller: inputCtrl,
@@ -58,40 +53,41 @@ class _MonthlyCalculationState extends State<MonthlyCalculation> {
                           },
                         ),
                       ),
-                    ],
-                  ),
-                  itemWrap(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text('专项附加扣除', style: GlobalStyle.tipText),
-                        Row(
+                      Container(
+                        padding: MonthlyCalculationStyle.itemPadding,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.only(right: 5),
-                              child: Text(
-                                excludeMoney.toString(),
-                                style: MonthlyCalculationStyle.moreText,
-                              ),
+                            Text('专项附加扣除', style: GlobalStyle.tipText),
+                            Row(
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 5),
+                                  child: Text(
+                                    excludeMoney.toString(),
+                                    style: MonthlyCalculationStyle.moreText,
+                                  ),
+                                ),
+                                Image.asset(
+                                  'assets/circle-right.png',
+                                  width: 18,
+                                  height: 18,
+                                  fit: BoxFit.contain,
+                                )
+                              ],
                             ),
-                            Image.asset(
-                              'assets/circle-right.png',
-                              width: 18,
-                              height: 18,
-                              fit: BoxFit.contain,
-                            )
                           ],
                         ),
-                      ],
-                    ),
-                  ),
-                  // 收入概览
-                  IncomeOverview(income: 1000),
+                      ),
+                      // 收入概览
+                      IncomeOverview(income: 1000),
+                    ],
+                  )
                 ],
               ),
             ),
           ),
-          Text('Placeholder'),
+          SpecialAttachment(),
         ],
       ),
     );
---
+++ lib/src/views/MonthlyCalculation/SpecialAttachment.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'MonthlyCalculation.style.dart' as MonthlyCalculationStyle;
+
+class SpecialAttachment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        padding: MonthlyCalculationStyle.container,
+        child: Text('placeholder'),
+      ),
+    );
+  }
+}
