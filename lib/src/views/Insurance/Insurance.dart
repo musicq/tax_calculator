b26d475b8586f2c2c4ac3941c4367a74036c1492
musicq <liang.kui@hotmail.com>
2019-07-29 13:57:15 +0800

    feat: 使用 selectButtonGroup 组件作为类型选择

--- lib/src/views/Insurance/Insurance.dart
+++ lib/src/views/Insurance/Insurance.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:tax_calculator/src/widgets/InputField/InputField.dart';
 import 'package:tax_calculator/src/widgets/MainContainer/MainContainer.dart';
+import 'package:tax_calculator/src/widgets/SelectButtonGroup/SelectButtonGroup.dart';
+
+import 'Insurance.style.dart' as InsuranceStyle;
+
+const _actions = [
+  SelectButtonGroupItem(
+    label: '最高',
+    type: SelectButtonType.Highest,
+  ),
+  SelectButtonGroupItem(
+    label: '最低',
+    type: SelectButtonType.Lowest,
+  ),
+  SelectButtonGroupItem(
+    label: '不缴纳',
+    type: SelectButtonType.No,
+  ),
+  SelectButtonGroupItem(
+    label: '自定义',
+    type: SelectButtonType.Customize,
+  ),
+];
 
 class Insurance extends StatefulWidget {
   @override
@@ -7,10 +30,38 @@ class Insurance extends StatefulWidget {
 }
 
 class _InsuranceState extends State<Insurance> {
+  final _inputCtrl = TextEditingController();
+  final _selectedType = SelectButtonType.Lowest;
+
   @override
   Widget build(BuildContext context) {
     return MainContainer(
-      body: Text('社保'),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Text('社保缴纳', style: InsuranceStyle.title),
+            ),
+            InputField(
+              showTip: false,
+              label: '输入社保缴纳基数',
+              controller: _inputCtrl,
+              //              onSubmit: (String v) => _providentFund.setBasis(D(v)),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: SelectButtonGroup(
+                actions: _actions,
+                selectedType: _selectedType,
+                onSelected: (t) => print(t),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
